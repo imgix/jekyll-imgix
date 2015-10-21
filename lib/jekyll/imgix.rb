1ac263de20db43f117f5524d0187095f63735c49
kellysutton <michael.k.sutton@gmail.com>
2015-10-20 17:33:09 -0700

    Initial commit of the imgix Jekyll plugin

---
+++ lib/jekyll/imgix.rb
@@ -0,0 +1,7 @@
+require "jekyll/imgix/version"
+
+module Jekyll
+  module Imgix
+    # Your code goes here...
+  end
+end
---
+++ lib/jekyll/imgix/version.rb
@@ -0,0 +1,5 @@
+module Jekyll
+  module Imgix
+    VERSION = "0.1.0"
+  end
+end
